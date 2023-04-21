9b1d0de05f087c432cc2311ee975108463738877
zzh315 <zhengzh315@gmail.com>
2023-04-22 03:30:36 +1000

    added firebase API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useSyncExternalStore } from "react";
-
+import React, { useState, useEffect, useCallback } from "react";
+import AddMovie from "./components/AddMovie";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -7,32 +7,70 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMoviesHandler() {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch(
+        "https://react-movie-cbd13-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+      );
       if (!response.ok) {
         throw new Error(`${response.status} Something went wrong.`);
       }
-      console.log(response);
+
       const data = await response.json(); //.json parse is also asynchronous
       //await would return the result of the promise instead of returning the promise itself
+      console.log(data);
+      const loadedMovies = [];
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          releaseDate: movieData.release_date,
-          openingText: movieData.opening_crawl,
-        };
-      });
+      for (let key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          releaseDate: data[key].releaseDate,
+          openingText: data[key].openingText,
+        });
+      }
 
-      setMovies(transformedMovies);
+      // const transformedMovies = key.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     releaseDate: movieData.release_date,
+      //     openingText: movieData.opening_crawl,
+      //   };
+      // });
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]); //becasue arrow function have no hoisting. only can call after decalration
+
+  async function addMovieHandler(movie) {
+    try {
+      const response = await fetch(
+        "https://react-movie-cbd13-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+        {
+          method: "POST",
+          body: JSON.stringify(movie),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setError(error.messgae);
+    }
+    fetchMoviesHandler();
   }
 
   let content = <p>No movie found.</p>;
@@ -47,6 +85,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
